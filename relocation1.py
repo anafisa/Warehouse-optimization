fe2b80092a48695fbb81c26cdf7c6abd37d4295f
Anfisa <anfisa_lepekhina5@mail.ru>
2019-11-03 11:51:28 +0300

    add some functions

--- relocation1.py
+++ relocation1.py
@@ -1,20 +1,30 @@
-from import1 import wave_id, wave_floor_route, waves
+from import1 import wave_workers, wave_floor_route, floors, waves
 from route_time_calc import route_time
 from collections import defaultdict
 
+k = 0
+res = []
+final = []
+workers = []
+wave_time = defaultdict(lambda: [])
 
-def ordering1(workers, routes):
+
+def ordering1(n, routes):
+    """
+    Функция, которая по распределению рабочих по этажам,формирует пик-рейсы.
+    n - кол-во рабочих, работающих в определенную волну на определенном этаже по новому наспределению
+    routes - пик-рейсы, которые выполняются в определенную волну на определенном этаже
+    """
+
     d = dict()
-    new_time = [0] * workers  # время, полученное с учетом нового распределения пик-рейсов, изначально список заполнен нулями
+    new_time = [0] * n  # время, полученное с учетом нового распределения пик-рейсов, изначально список заполнен нулями
     time = []  # время каждого пик рейса
-    new_ordering = defaultdict(lambda: [])
-
 
     for i in routes:
-        d[route_time[i]] = i
-        time.append(route_time[i])  # словарь вида время : пикрейс
+        d[route_time[i]] = i  # словарь вида время : пикрейс
+        time.append(route_time[i])
 
-    time = sorted(time, reverse=True)  # сортируем в обратном порядке
+    time = sorted(time, reverse=True)
 
     while time:  # пока есть нераспределенные пик-рейсы
         for i in range(0, len(new_time)):
@@ -27,52 +37,38 @@ def ordering1(workers, routes):
     return new_time
 
 
-wave_workers = dict()
-for i in wave_id.keys():  # составляю словарь волна : кол-во рабочих
-    wave_workers[i] = len(wave_id[i].keys())
+def floor_ordering(time_lst, n):
+    """
+    Функция распределения рабочих по этажам пропорционально времени.
+    n - кол-во всех рабочих по волне,
+    time_lst - список времени работы каждого этажа
+    """
+    s = sum(time_lst)
+    d = s / n
+    ordering = []
+    for t in time_lst:
+        ordering.append(round(t / d))
+    if sum(ordering) > n:
+        ordering[-1] -= 1
+    return ordering
 
 
-
-time = defaultdict(lambda: [])
-f = ['1', '2', '3', '4']
-
-
+# находим время выполнения каждой волны на каждом этаже
 for i in waves:
-    for j in f:
+    for j in floors:
         c = 0
         routes = wave_floor_route[i][j]
         for r in routes:
             c += route_time[r]
-        time[i].append(c)  # находим время выполнения каждой волны
+        wave_time[i].append(c)
 
-
-# print(time)
-# print(wave_workers)
-
-
-def floor_ordering(lst, n):
-    s = sum(lst)
-    d = s / n
-    res = []
-    for i in lst:
-        res.append(round(i / d))
-    if sum(res) > n:
-        res[-1] -= 1
-    return res
-
-
-workers = []
+# распределяем рабочих по этажам
 for w in waves:
-    workers.append(floor_ordering(time[w], wave_workers[w]))
-
-# print(workers)
+    workers.append(floor_ordering(wave_time[w], wave_workers[w]))
 
 
-k = 0
-res = []
-final = []
 for j in workers:
-    for i in f:
+    for i in floors:
             w = j[int(i)-1]
             routes = wave_floor_route[waves[k]][i]
             res.append(max(ordering1(w, routes)))

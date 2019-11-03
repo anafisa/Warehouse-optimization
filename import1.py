f997b7bbfe4028cd47b28aa1bcd74d9b0ca86a71
Anfisa <anfisa_lepekhina5@mail.ru>
2019-11-03 11:50:31 +0300

    add floors

--- import1.py
+++ import1.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from pprint import pprint
 from openpyxl import load_workbook
 from collections import defaultdict
@@ -9,7 +8,11 @@ sheet = wb['Лист1']
 
 waves = []
 
-route_num = defaultdict(lambda: [])
+floors = []
+
+wave_workers = dict()
+
+route_num = defaultdict(lambda: 0)
 
 route_cell = defaultdict(lambda: [])
 id_route = defaultdict(lambda: route_cell.copy())
@@ -26,6 +29,10 @@ wave_floor_route = defaultdict(lambda: floor_route.copy())
 floor_id = defaultdict(lambda: [])
 wave_floor_id = defaultdict(lambda: floor_id.copy())
 
+idroute = defaultdict(lambda: [])
+waveid = defaultdict(lambda: idroute.copy())
+floor_wave_id_route = defaultdict(lambda: waveid.copy())
+
 
 for i in range(1, 55432):
     wave = sheet.cell(row=i, column=2).value
@@ -35,16 +42,19 @@ for i in range(1, 55432):
     num = sheet.cell(row=i, column=5).value
     floor = cell[0]
 
-    if wave not in waves:
-        waves.append(wave)
-
     wave_id[wave][id][route].append(list(map(int, cell[1:-2].split("-"))))  # {wave1:{id1:[{route1:[cell1,cell2]}], id2}
 
     wave_id_num[wave][id].append(num)  # {wave1:{id1:[num1,num2],id2} - number of goods which picks each person (id)
 
     route_cell_d[route].append(cell)  # {route1:[cell1, cell2], route2:[]}
 
-    route_num[route].append(num)
+    route_num[route] += num
+
+    if floor not in floors:
+        floors.append(floor)
+
+    if wave not in waves:
+        waves.append(wave)
 
     if route not in wave_floor_route[wave][floor]:
         wave_floor_route[wave][floor].append(route)  # {wave1:{'1':[r1,r2]}, wave2:}
@@ -52,10 +62,12 @@ for i in range(1, 55432):
     if id not in wave_floor_id[wave][floor]:
         wave_floor_id[wave][floor].append(id)  # wave1:{'1':[id1,id2]}, wave2:}
 
-# pprint(route_num)
-# pprint(wave_id_num)
-# print(wave_id)
+    if route not in floor_wave_id_route[floor][wave][id]:
+        floor_wave_id_route[floor][wave][id].append(route)
 
+for j in wave_id.keys():
+    wave_workers[j] = len(wave_id[j].keys())
 
+floors.sort()
 
 

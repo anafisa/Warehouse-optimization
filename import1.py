3b98bae725cd923a7caeede916e21fa244d9301b
Anfisa <anfisa_lepekhina5@mail.ru>
2019-11-23 14:46:03 +0300

    upd cell

--- import1.py
+++ import1.py
@@ -38,6 +38,10 @@ route_box_cell = defaultdict(lambda: box_cell_lst.copy())
 floor_route = defaultdict(lambda: [])
 wave_floor_route = defaultdict(lambda: floor_route.copy())
 
+route_box = defaultdict(lambda: [])
+floor_route_box = defaultdict(lambda: route_box.copy())
+wave_floor_route_box = defaultdict(lambda: floor_route_box.copy())
+
 floor_id = defaultdict(lambda: [])
 wave_floor_id = defaultdict(lambda: floor_id.copy())
 
@@ -57,7 +61,7 @@ for i in range(1, 55432):
 
     wave_id[wave][id][route].append(list(map(int, cell[1:-2].split("-"))))  # {wave1:{id1:[{route1:[cell1,cell2]}], id2}
 
-    wave_floor_box_cell[wave][floor][box].add(int(cell[1:3]))
+    wave_floor_box_cell[wave][floor][box].add((int(cell[1:3])-1)//2)
 
     route_box_cell[route][box].append(int(cell[1:3]))
 
@@ -67,7 +71,7 @@ for i in range(1, 55432):
 
     route_cell_d[route].append(cell)  # {route1:[cell1, cell2], route2:[]}
 
-    box_cell_d[box].append(cell)
+    box_cell_d[box].append(int(cell[1:3]))
 
     route_num[route] += num
 
@@ -87,6 +91,10 @@ for i in range(1, 55432):
 
     if route not in floor_wave_id_route[floor][wave][id]:
         floor_wave_id_route[floor][wave][id].append(route)
+
+    if box not in wave_floor_route_box[wave][floor][route]:
+        wave_floor_route_box[wave][floor][route].append(box)
+
 
 for j in wave_id.keys():
     wave_workers[j] = len(wave_id[j].keys())

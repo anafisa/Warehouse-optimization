16f0c484bddffef95667f0208f8f877c6d702f25
Anfisa <anfisa_lepekhina5@mail.ru>
2019-11-28 18:15:53 +0300

    upd data

--- route_time_calc.py
+++ route_time_calc.py
@@ -7,31 +7,34 @@ wave_time = defaultdict(lambda: [])  # время выполнения кажд�
 
 def sshape(route_cell, route_num):
     r_time = dict()
-    last = 0
     for route in route_cell.keys():
         k = []
         for i in route_cell[route]:
             cell = list(map(int, i[1:-2].split("-")))
             if (cell[0] - 1) // 2 not in k:
                 k.append((cell[0] - 1) // 2)
-                last = cell[1]
+
         if route_floor[route] == '1':
-           r_time[route] = ((len(k) * 27 + (max(k) + 1) * 5.1 + last * 0.9) / 0.89
-                            + route_num[route] * 2.37) / 60
+            r_time[route] = (((len(k)+1) * 27 + (len(k) - 1) * 5.1) / 0.89
+                                + route_num[route] * 2.37) / 60
+
         elif route_floor[route] == '3':
-            r_time[route] = ((len(k) * 24 + (max(k) + 1) * 5.1 + last * 0.6) / 0.89
+            r_time[route] = (((len(k)+1) * 24 + (len(k) - 1) * 5.1) / 0.89
                              + route_num[route] * 2.37) / 60
         else:
-            r_time[route] = ((len(k) * 25.2 + (max(k) + 1) * 5.1 + last * 0.63) / 0.89
+            r_time[route] = (((len(k)+1) * 25.2 + (len(k) - 1) * 5.1) / 0.89
                              + route_num[route] * 2.37) / 60
-
 
     return r_time
 
 
 route_time = sshape(route_cell_d, route_num)
-print(route_time[1187785])
-print(route_cell_d[1187785])
+# print(route_time[1187705])
+# print(route_time[1189361])
+# print(route_time[1189711])
+# print(route_time[1189722])
+
+
 
 
 for i in waves:
@@ -42,5 +45,6 @@ for i in waves:
             c += route_time[r]
         wave_time[i].append(c)
 
-print(wave_time)
-
+# print(wave_time)
+#  r_time[route] = ((len(k) * 27 + (max(k) + 1) * 5.1) / 0.89
+#                             + route_num[route] * 2.37) / 60
